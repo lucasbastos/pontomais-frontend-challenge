76c79213994996bebd2a1d09eb5cc66443d94baa
Lucas Bastos <lucasbastos.ecomp@gmail.com>
2022-06-12 14:17:02 -0300

    only html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pontomais-frontend-challenge';
+  personagens = [
+    {
+      nome: 'Luke Skywalker',
+      imagem: 'https://starwars-visualguide.com/assets/img/characters/luke-skywalker-wallpaper-10.jpg'
+    },
+    {
+      nome: 'C-3PO',
+      imagem: 'https://starwars-visualguide.com/assets/img/characters/c-3po-wallpaper-10.jpg'
+    },
+    {
+      nome: 'R2-D2',
+      imagem: 'https://starwars-visualguide.com/assets/img/characters/r2-d2-wallpaper-10.jpg'
+    }
+  ];
 }
